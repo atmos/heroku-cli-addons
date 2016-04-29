aa8ca55dec56d29ce19860ba4806a0031559c378
Ransom Briggs <rbriggs@heroku.com>
2016-04-29 12:34:55 -0500

    Adding tests for config_vars & attachments

--- test/commands/addons/destroy.js
+++ test/commands/addons/destroy.js
@@ -23,7 +23,9 @@ describe('addons:destroy', () => {
     addon_service: {name: 'heroku-redis'},
     app: {id: '4321-8765', name: 'myapp'},
     plan: {name: 'premium-0', price: {cents: 1, unit: 'month'}},
-    config_vars: []
+    config_vars: [
+      'REDIS_VAR'
+    ]
   }
 
   it('does not destroy addons with no confirm', () => {
@@ -47,10 +49,21 @@ describe('addons:destroy', () => {
       .get('/apps/myapp/addons/redis-swiftly-123').reply(200, addon)
 
     let attachApi = nock('https://api.heroku.com:443')
-      .get('/addons/redis-swiftly-123/addon-attachments').reply(200, [])
+      .get('/addons/redis-swiftly-123/addon-attachments')
+      .reply(200, [{
+        name: 'redis-attachly-456',
+        app: {
+          name: 'otherapp'
+        }
+      }])
 
     let addonDelete = nock('https://api.heroku.com:443')
       .delete('/apps/4321-8765/addons/1234-5678').reply(200, addon)
+
+    let attachedRelease = nock('https://api.heroku.com:443')
+      .get('/apps/otherapp/releases').reply(200, [{
+        version: 10
+      }])
 
     cli.confirmApp = function *() { }
 
@@ -58,6 +71,12 @@ describe('addons:destroy', () => {
     .then(() => addonApi.done())
     .then(() => attachApi.done())
     .then(() => addonDelete.done())
+    .then(() => attachedRelease.done())
+    .then(() => expect(cli.stdout, 'to equal', ''))
+    .then(() => expect(cli.stderr, 'to equal',
+`Destroying redis-swiftly-123 on myapp... $0.01/month
+Removing config vars redis-attachly-456 on otherapp and restarting... done, v10
+`))
   })
 
   it('destroys addons with confirm option', () => {
